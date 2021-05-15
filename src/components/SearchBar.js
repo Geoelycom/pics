19744d6b0300c1c20dce43d713407e122da22c8a
ekenimoh elyan <ekenimohelyanpheta@gmail.com>
2021-05-15 23:18:12 +0100

    new feature added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,10 +2,15 @@ import React from 'react';
 class SearchBar extends React.Component {
     state = {term: ''}
 
+onFormSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.term)
+}
+
     render() {
         return (
         <div className="ui segment">
-            <form className="ui form">
+            <form  onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                    <label>Image Search</label> 
                     <input type="text" value={this.state.term} onChange={e => this.setState({term: e.target.value})}/>
@@ -19,3 +24,4 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+ 
